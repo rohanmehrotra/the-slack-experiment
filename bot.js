5a28ec6665910c9353cdbf8a28aa30680a0fdc3e
Sanket <sanketpandia@gmail.com@users.noreply.github.com>
2021-02-18 15:05:45 +0530

    Added auth

--- bot.js
+++ bot.js
@@ -15,6 +15,27 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/auth/redirect', (req, res) =>{
+	var options = {
+  		uri: 'https://slack.com/api/oauth.access?code='
+  			+req.query.code+
+  			'&client_id='+process.env.CLIENT_ID+
+  			'&client_secret='+process.env.CLIENT_SECRET+
+  			'&redirect_uri='+process.env.REDIRECT_URI,
+		method: 'GET'
+  	}
+  	request(options, (error, response, body) => {
+  		var JSONresponse = JSON.parse(body)
+  		if (!JSONresponse.ok){
+  			console.log(JSONresponse)
+  			res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+  		}else{
+  			console.log(JSONresponse)
+  			res.send("Success!")
+  		}
+  	})
+})
+
 app.post('/live', async (req, res) => {
     // We will be coding here
     console.log(req.protocol + '://' + req.get('Host') + req.url);
